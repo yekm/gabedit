d9a27951be25532b05dfc1b0e9ac55eb9d85b523
allouchear <allouchear@0d66876f-9eec-48b2-9bc5-898b2d12366d>
2009-11-18 19:57:47 +0000

    git-svn-id: https://svn.code.sf.net/p/gabedit/code@25 0d66876f-9eec-48b2-9bc5-898b2d12366d

--- GabeditSrc225/src/PCGamess/PCGamessSCF.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GABEDIT_PCGAMESSSCF_H__
-#define __GABEDIT_PCGAMESSSCF_H__
-
-void initPCGamessSCFFrame();
-void setSensitivePCGamessSCFFrame(gboolean sensitive);
-void putPCGamessSCFInfoInTextEditor();
-void createPCGamessSCFFrame(GtkWidget *win, GtkWidget *box);
-
-#endif /* __GABEDIT_PCGAMESSSCF_H__ */
