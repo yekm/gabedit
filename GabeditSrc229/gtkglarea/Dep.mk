499a9c6acd3b21a5a607cfacabf538defbc624d0
allouchear <allouchear@0d66876f-9eec-48b2-9bc5-898b2d12366d>
2010-04-08 15:35:37 +0000

    git-svn-id: https://svn.code.sf.net/p/gabedit/code@30 0d66876f-9eec-48b2-9bc5-898b2d12366d

--- GabeditSrc229/gtkglarea/Dep.mk
+++
@@ -1,7 +0,0 @@
-# DO NOT DELETE
-
-gdkgl-win32.o: gdkgl.h
-gdkgl.o: gdkgl.h
-gtkglarea.o: gdkgl.h gtkglarea.h
-gtkglarea.o: ../Config.h
-gtkglarea.o: gdkgl.h
--- GabeditSrc229/src/PCGamess/PCGamessBasis.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GABEDIT_PCGAMESSBASIS_H__
-#define __GABEDIT_PCGAMESSBASIS_H__
-
-void initPCGamessBasisFrame();
-void setSensitivePCGamessBasisFrame(gboolean sensitive);
-void putPCGamessBasisInfoInTextEditor();
-void createPCGamessBasisFrame(GtkWidget *win, GtkWidget *box);
-
-#endif /* __GABEDIT_PCGAMESSBASIS_H__ */
