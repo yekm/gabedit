cab7aa501ce2dee7929c654361f1675c6aedfcd6
allouchear <allouchear@0d66876f-9eec-48b2-9bc5-898b2d12366d>
2010-05-06 07:45:27 +0000

    git-svn-id: https://svn.code.sf.net/p/gabedit/code@33 0d66876f-9eec-48b2-9bc5-898b2d12366d

--- GabeditSrc2211/src/Geometry/FragmentsTree.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __GABEDIT_FRAGMENTSTREE_H__
-#define __GABEDIT_FRAGMENTSTREE_H__
-
-GtkWidget* addFragmentsTreeView(GtkWidget *box);
-void rafreshTreeView(GtkWidget *treeView);
-
-#endif /* __GABEDIT_FRAGMENTSTREE_H__ */
-
