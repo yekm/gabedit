3e9e7dbfc2c890e673887feee5d2a704bd4ffa05
allouchear <allouchear@0d66876f-9eec-48b2-9bc5-898b2d12366d>
2010-12-21 19:06:04 +0000

    git-svn-id: https://svn.code.sf.net/p/gabedit/code@46 0d66876f-9eec-48b2-9bc5-898b2d12366d

--- GabeditSrc232/src/Spectrum/IRSpectrum.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef __GABEDIT_IRSPECTRUM_H__
-#define __GABEDIT_IRSPECTRUM_H__
-
-void createIRSpectrum(GtkWidget *parentWindow, GabEditTypeFile typeOfFile);
-void createIRSpectrumFromVibration(GtkWidget *parentWindow, Vibration ibration);
-
-#endif /* __GABEDIT_IRSPECTRUM_H__ */
